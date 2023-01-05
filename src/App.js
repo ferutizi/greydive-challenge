174d8a21c2c833c1b8df1cb42ade91000ccb611c
ferutizi <fer.utizi@gmail.com>
2023-01-05 16:27:22 -0300

    state form, handleChange, handleSubmit - fixed submit button label

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
+import { useState } from 'react';
 import data from './db.json';
 
 function App() {
+  const [formulario, setFormulario] = useState({});
+  
+  const handleChange = (e) => {
+    setFormulario({
+      ...formulario,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formulario);
+  }
+
   return (
     <>
-      {data.items.map(item => 
-        item.type == 'select' ? 
-          <>
-            <label>{item.label}</label>
-            <select name={item.name}>
-              {item.options.map(option => 
-                <option value={option.value}>{option.label}</option>
-                )}
-            </select>
-          </>
-        :
-          <div key={item.label}>
-            <label>{item.label}</label>
-            <input type={item.type}></input>
-          </div>
-        )}
+      <form onSubmit={handleSubmit}>
+        {data.items.map(item => 
+          item.type == 'select' ? 
+            <>
+              <label key={item.label}>{item.label}</label>
+              <select name={item.name}>
+                {item.options.map(option => 
+                  <option key={option.label} value={option.value}>{option.label}</option>
+                  )}
+              </select>
+            </>
+          :
+            <div key={item.label}>
+              <label>{item.type == 'submit' ? '' : item.label}</label>
+              <input type={item.type} name={item.name} value={item.value} onChange={handleChange} />
+            </div>
+          )
+        }
+      </form>
     </>
   );
 }
