331f0e4b60773255d449c8e4aef8e85d2ff7ed2b
ferutizi <fer.utizi@gmail.com>
2023-01-06 15:22:54 -0300

    Button disabled when user doesn't check term and conditions on true

--- src/App.js
+++ src/App.js
@@ -3,8 +3,12 @@ import data from './db.json';
 
 function App() {
   const [formulario, setFormulario] = useState({});
+  const [checked, setChecked] = useState(false);
   
   const handleChange = (e) => {
+    if(e.target.type === 'checkbox') {
+      setChecked(!checked)
+    }
     setFormulario({
       ...formulario,
       [e.target.name]: e.target.value
@@ -20,20 +24,28 @@ function App() {
     <>
       <form onSubmit={handleSubmit}>
         {data.items.map(item => 
-          item.type == 'select' ? 
-            <>
-              <label key={item.label}>{item.label}</label>
+          item.type === 'select' ?
+            <div key={item.label}>
+              <label>{item.label}</label>
               <select name={item.name}>
                 {item.options.map(option => 
                   <option key={option.label} value={option.value}>{option.label}</option>
                   )}
               </select>
-            </>
+            </div>
           :
             <div key={item.label}>
-              <label>{item.type == 'submit' ? '' : item.label}</label>
-              <input type={item.type} name={item.name} value={item.value} onChange={handleChange} />
+              <label>{item.type === 'submit' ? '' : item.label}</label>
+              <input
+                type={item.type}
+                name={item.name}
+                value={item.value}
+                onChange={handleChange}
+                checked={checked}
+                disabled={item.type === 'submit' ? !checked : false}
+                />
             </div>
+            
           )
         }
       </form>
